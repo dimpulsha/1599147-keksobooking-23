68f0b925634e64452dbc92fd0ba05dc51f17ade5
dimpulsha <dimpulsha@gmail.com>
2021-06-20 20:18:26 +0500

    функции вывода информции о комнатах и гостях исправлены, удалены лишние комментарии

--- js/main.js
+++ js/main.js
@@ -1,12 +1,6 @@
 import { getTestData } from './utils/create-test-data.js';
 import { createOfferCard } from './map-offer-card.js';
 import { createOfferCardList } from './map-offer-card.js';
-// import './map/map.js'; // работа с картой
-// import './compare-offer/compare-offer.js'; // операции по сравнению объявлений, фильтры там и т.п
-// import './compare-offer/get-server-data.js'; // получение объявлений с сервера
-// import './compare-offer/offer-card.js'; // отображение карточки объявления
-// import './create-offer/fill-offer-form.js'; // заполнение новой карточки и отправка
-// import './create-offer/check-form.js'; // проверка правильности заполнения формы
 
 const TEST_OBJECT_NUM = 3;
 const offerList = getTestData(TEST_OBJECT_NUM);
@@ -17,4 +11,3 @@ mapField.appendChild(createOfferCard(offerList[1]));
 
 //вставка списка карточек на поле с картой
 mapField.appendChild(createOfferCardList(offerList));
-

bafa7dc7ede9604fdaf96faa36e030c689653bf9
DimPulsha <72344658+dimpulsha@users.noreply.github.com>
2021-07-17 01:35:54 +0500

    Update map-settings.js

--- js/map/map-settings.js
+++ js/map/map-settings.js
@@ -12,13 +12,13 @@ const MAP_LAYER = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 const MAP_ATTRIBUTION = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
 
 const MAP_MAIN_ICON = {
-  iconUrl: '../../img/main-pin.svg',
+  iconUrl: '/img/main-pin.svg',
   iconSize: [52, 52],
   iconAnchor: [26, 52],
 };
 
 const MAP_ICON = {
-  iconUrl: '../../img/pin.svg',
+  iconUrl: '/img/pin.svg',
   iconSize: [40, 40],
   iconAnchor: [20, 40],
 };
